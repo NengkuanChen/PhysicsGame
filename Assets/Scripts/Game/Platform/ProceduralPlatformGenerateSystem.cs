e32701597949114c13c7d568fc019f8ef45ffb01
Nengkuan Chen <cnk99@live.cn>
2022-11-18 02:14:49 -0500

    PlatformGeneration

--- Assets/Scripts/Game/Platform/ProceduralPlatformGenerateSystem.cs
+++ Assets/Scripts/Game/Platform/ProceduralPlatformGenerateSystem.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Game.Entity;
+using Game.GameEvent;
 using Game.GameSystem;
+using Game.Scene;
 using Game.Utility;
+using GameFramework.Event;
+using Table;
+using UnityEngine;
 
 namespace Game.PlatForm
 {
@@ -16,22 +21,86 @@ namespace Game.PlatForm
             return SystemEntry.GetSystem(UniqueId) as ProceduralPlatformGenerateSystem;
         }
 
-        private int currentTableId = 0;
+        private int currentTableId = 1;
         
         private List<PlatformGroupEntity> platformGroupEntities = new List<PlatformGroupEntity>();
         public List<PlatformGroupEntity> PlatformGroupEntities => platformGroupEntities;
+        
+        private List<PlatformGroupEntity> playerPassedEntities = new List<PlatformGroupEntity>();
+        public List<PlatformGroupEntity> PlayerPassedEntities => playerPassedEntities;
+
+        private PlatformSetting setting;
+
+
 
         internal override void OnEnable()
         {
             base.OnEnable();
+            Framework.EventComponent.Subscribe(OnPlayerEnterPlatformGroupEventArgs.UniqueId, OnPlayerEnterPlatformGroup);
+            setting = SettingUtility.PlatformSetting;
+            for (int i = 0; i < setting.PlatformFront; i++)
+            {
+                RandomGeneratePlatformGroup().Forget();
+            }
+        }
+
+        private void OnPlayerEnterPlatformGroup(object sender, GameEventArgs e)
+        {
+            var arg = e as OnPlayerEnterPlatformGroupEventArgs;
+            if (!playerPassedEntities.Contains(arg.PlatformGroup))
+            {
+                playerPassedEntities.Add(arg.PlatformGroup);
+            }
+
+            var platFront = platformGroupEntities.Count - platformGroupEntities.IndexOf(arg.PlatformGroup);
+            if (platFront < setting.PlatformFront)
+            {
+                RandomGeneratePlatformGroup().Forget();
+            }
+
+
+            if (playerPassedEntities.Count > setting.PlatformBehind)
+            {
+                var entity = playerPassedEntities[0];
+                playerPassedEntities.RemoveAt(0);
+                platformGroupEntities.Remove(entity);
+                entity.Hide();
+            }
+
+            
         }
 
         public async UniTask<PlatformGroupEntity> GeneratePlatformGroup(int platformID)
         {
             var newPlatform =
                 await EntityUtility.ShowEntityAsync<PlatformGroupEntity>($"PlatformGroup/{platformID}", EntityGroupName.Platform);
+            newPlatform.transform.parent = ScrollRoot.Current.transform;
+            if (platformGroupEntities.Count > 0)
+            {
+                newPlatform.transform.position += (platformGroupEntities[^1].ExitPoistion.position -
+                                                   newPlatform.EnterPoistion.position);
+            }
+            else
+            {
+                newPlatform.transform.position += (ScrollRoot.Current.transform.position -
+                                                   newPlatform.EnterPoistion.position + Vector3.down * 5);
+            }
             platformGroupEntities.Add(newPlatform);
             return newPlatform;
+        }
+
+        public async UniTask<PlatformGroupEntity> RandomGeneratePlatformGroup()
+        {
+            int[] currentPlatformIds = PlatformGenerationTable.GetValueOrThrErr(currentTableId).PlatformIdArray;
+            int randomIndex = Random.Range(0, currentPlatformIds.Length);
+            int platformId = currentPlatformIds[randomIndex];
+            return await GeneratePlatformGroup(platformId);
+        }
+
+        internal override void OnDisable()
+        {
+            base.OnDisable();
+            Framework.EventComponent.Unsubscribe(OnPlayerEnterPlatformGroupEventArgs.UniqueId, OnPlayerEnterPlatformGroup);
         }
     }
 }
