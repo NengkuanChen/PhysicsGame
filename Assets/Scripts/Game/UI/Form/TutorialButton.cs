d47a51f7c42bcb79ea66194bdf33ddaea000a368
Nengkuan Chen <cnk99@live.cn>
2022-12-04 22:03:10 -0500

    TutorialForm

--- Assets/Scripts/Game/UI/Form/TutorialButton.cs
+++ Assets/Scripts/Game/UI/Form/TutorialButton.cs
@@ -1,7 +1,31 @@
-﻿namespace Game.UI.Form
+﻿using Game.GameEvent;
+using Game.Utility;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI.Form
 {
-    public class TutorialButton
+    public class TutorialButton: StaticUIElement
     {
+        [SerializeField] 
+        private Button tutorialButton;
+        
+
+        public override void OnInit(object userData)
+        {
+            base.OnInit(userData);
+            tutorialButton.onClick.AddListener(() =>
+            {
+                // Framework.EventComponent.Fire(this, OnGamePauseEventArgs.Create(true));
+                if (UIUtility.GetForm(TutorialForm.UniqueId) != null)
+                {
+                    return;
+                }
+                UIUtility.OpenForm(TutorialForm.UniqueId);
+                tutorialButton.interactable = false;
+            });
+        }
         
     }
 }
